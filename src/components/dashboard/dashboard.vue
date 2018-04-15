30c3fa8b258f01cd820983859166b72517b68d2a
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-15 23:17:25 +0200

    337 setting global request configuration

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,47 +1,47 @@
 <template>
-  <div id="dashboard">
-    <h1>That's the dashboard!</h1>
-    <p>You should only get here if you're authenticated!</p>
-    <p>Your Email: {{ email }}</p>
-  </div>
+    <div id="dashboard">
+        <h1>That's the dashboard!</h1>
+        <p>You should only get here if you're authenticated!</p>
+        <p>Your Email: {{ email }}</p>
+    </div>
 </template>
 
 <script>
-import axios from "axios";
+    import axios from "axios";
 
-export default {
-  data() {
-    return {
-      email: ""
-    };
-  },
-  created() {
-    axios
-      .get("https://vue-axios-62ced.firebaseio.com/users.json")
-      .then(res => {
-        console.log(res);
-        const data = res.data;
-        var users = [];
-        for (let key in data) {
-          const user = data[key];
-          user.id = key;
-          users.push(user);
+    export default {
+        data() {
+            return {
+                email: ""
+            };
+        },
+        created() {
+            axios
+                .get("/users.json")
+                .then(res => {
+                    console.log(res);
+                    const data = res.data;
+                    var users = [];
+                    for (let key in data) {
+                        const user = data[key];
+                        user.id = key;
+                        users.push(user);
+                    }
+                    this.email = users[0].email;
+                })
+                .catch(err => console.error(err));
         }
-        this.email = users[0].email;
-      })
-      .catch(err => console.error(err));
-  }
-};
+    };
 </script>
 
 
 <style scoped>
-h1,
-p {
-  text-align: center;
-}
+    h1,
+    p {
+        text-align: center;
+    }
 
-p {
-  color: red;
-}
+    p {
+        color: red;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './router'
 import store from './store'
 
+axios.defaults.baseURL = 'https://vue-axios-62ced.firebaseio.com';
+axios.defaults.headers.common['Authorization'] = 'Basic wtrgt4r564gtdgt4';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
 new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
