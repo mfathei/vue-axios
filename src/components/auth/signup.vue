6a39bf2e84e355a3ff0b38d9484cfad9c08bc371
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-15 22:45:48 +0200

    334 sending a POST request

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -69,129 +69,135 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        email: '',
-        age: null,
-        password: '',
-        confirmPassword: '',
-        country: 'usa',
-        hobbyInputs: [],
-        terms: false
-      }
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: "",
+      age: null,
+      password: "",
+      confirmPassword: "",
+      country: "usa",
+      hobbyInputs: [],
+      terms: false
+    };
+  },
+  methods: {
+    onAddHobby() {
+      const newHobby = {
+        id: Math.random() * Math.random() * 1000,
+        value: ""
+      };
+      this.hobbyInputs.push(newHobby);
     },
-    methods: {
-      onAddHobby () {
-        const newHobby = {
-          id: Math.random() * Math.random() * 1000,
-          value: ''
-        }
-        this.hobbyInputs.push(newHobby)
-      },
-      onDeleteHobby (id) {
-        this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id)
-      },
-      onSubmit () {
-        const formData = {
-          email: this.email,
-          age: this.age,
-          password: this.password,
-          confirmPassword: this.confirmPassword,
-          country: this.country,
-          hobbies: this.hobbyInputs.map(hobby => hobby.value),
-          terms: this.terms
-        }
-        console.log(formData)
-      }
+    onDeleteHobby(id) {
+      this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id);
+    },
+    onSubmit() {
+      const formData = {
+        email: this.email,
+        age: this.age,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        country: this.country,
+        hobbies: this.hobbyInputs.map(hobby => hobby.value),
+        terms: this.terms
+      };
+      console.log(formData);
+      axios
+        .post("https://vue-axios-62ced.firebaseio.com/users.json", formData)
+        .then(res => console.log(res))
+        .catch(error => console.error(error));
     }
   }
+};
 </script>
 
 <style scoped>
-  .signup-form {
-    width: 400px;
-    margin: 30px auto;
-    border: 1px solid #eee;
-    padding: 20px;
-    box-shadow: 0 2px 3px #ccc;
-  }
+.signup-form {
+  width: 400px;
+  margin: 30px auto;
+  border: 1px solid #eee;
+  padding: 20px;
+  box-shadow: 0 2px 3px #ccc;
+}
 
-  .input {
-    margin: 10px auto;
-  }
+.input {
+  margin: 10px auto;
+}
 
-  .input label {
-    display: block;
-    color: #4e4e4e;
-    margin-bottom: 6px;
-  }
+.input label {
+  display: block;
+  color: #4e4e4e;
+  margin-bottom: 6px;
+}
 
-  .input.inline label {
-    display: inline;
-  }
+.input.inline label {
+  display: inline;
+}
 
-  .input input {
-    font: inherit;
-    width: 100%;
-    padding: 6px 12px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-  }
+.input input {
+  font: inherit;
+  width: 100%;
+  padding: 6px 12px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+}
 
-  .input.inline input {
-    width: auto;
-  }
+.input.inline input {
+  width: auto;
+}
 
-  .input input:focus {
-    outline: none;
-    border: 1px solid #521751;
-    background-color: #eee;
-  }
+.input input:focus {
+  outline: none;
+  border: 1px solid #521751;
+  background-color: #eee;
+}
 
-  .input select {
-    border: 1px solid #ccc;
-    font: inherit;
-  }
+.input select {
+  border: 1px solid #ccc;
+  font: inherit;
+}
 
-  .hobbies button {
-    border: 1px solid #521751;
-    background: #521751;
-    color: white;
-    padding: 6px;
-    font: inherit;
-    cursor: pointer;
-  }
+.hobbies button {
+  border: 1px solid #521751;
+  background: #521751;
+  color: white;
+  padding: 6px;
+  font: inherit;
+  cursor: pointer;
+}
 
-  .hobbies button:hover,
-  .hobbies button:active {
-    background-color: #8d4288;
-  }
+.hobbies button:hover,
+.hobbies button:active {
+  background-color: #8d4288;
+}
 
-  .hobbies input {
-    width: 90%;
-  }
+.hobbies input {
+  width: 90%;
+}
 
-  .submit button {
-    border: 1px solid #521751;
-    color: #521751;
-    padding: 10px 20px;
-    font: inherit;
-    cursor: pointer;
-  }
+.submit button {
+  border: 1px solid #521751;
+  color: #521751;
+  padding: 10px 20px;
+  font: inherit;
+  cursor: pointer;
+}
 
-  .submit button:hover,
-  .submit button:active {
-    background-color: #521751;
-    color: white;
-  }
+.submit button:hover,
+.submit button:active {
+  background-color: #521751;
+  color: white;
+}
 
-  .submit button[disabled],
-  .submit button[disabled]:hover,
-  .submit button[disabled]:active {
-    border: 1px solid #ccc;
-    background-color: transparent;
-    color: #ccc;
-    cursor: not-allowed;
-  }
+.submit button[disabled],
+.submit button[disabled]:hover,
+.submit button[disabled]:active {
+  border: 1px solid #ccc;
+  background-color: transparent;
+  color: #ccc;
+  cursor: not-allowed;
+}
 </style>
