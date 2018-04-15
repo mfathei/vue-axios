3bbf90da55dd80e96faa4716eb1cfae6be4718d8
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-15 23:30:25 +0200

    338 using interceptors

--- src/main.js
+++ src/main.js
@@ -9,9 +9,22 @@ axios.defaults.baseURL = 'https://vue-axios-62ced.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'Basic wtrgt4r564gtdgt4';
 axios.defaults.headers.get['Accepts'] = 'application/json';
 
+const reqInterceptor = axios.interceptors.request.use(config => {
+    console.log(config);
+    return config;
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+    console.log(res);
+    return res;
+});
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
 new Vue({
     el: '#app',
     router,
     store,
     render: h => h(App)
-})
+});
