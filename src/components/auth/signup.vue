4c3852fbb757e92870dc4341d40b1ec33e7c72c7
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-16 16:55:39 +0200

    369 using the required-unless validator

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -63,8 +63,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="input inline">
-                    <input type="checkbox" id="terms" v-model="terms">
+                <div class="input inline" :class="{invalid: $v.terms.$invalid}">
+                    <input type="checkbox" id="terms" v-model="terms"
+                           @change="$v.terms.$touch()">
                     <label for="terms">Accept Terms of Use</label>
                 </div>
                 <div class="submit">
@@ -77,7 +78,7 @@
 
 <script>
 
-    import {required, email, numeric, minValue, minLength, sameAs} from 'vuelidate/lib/validators';
+    import {required, email, numeric, minValue, minLength, sameAs, requiredUnless} from 'vuelidate/lib/validators';
 
     export default {
         data() {
@@ -109,6 +110,11 @@
                 // sameAs: sameAs('password')
                 sameAs: sameAs(vm => {
                     return vm.password;
+                })
+            },
+            terms: {
+                required: requiredUnless(vm => {
+                    return vm.country === 'germany';
                 })
             }
         },
